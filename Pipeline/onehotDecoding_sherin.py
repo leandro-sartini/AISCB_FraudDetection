cccecc588db8fadbd04e6875a12cd3fc8e4e203c
sherin stanley <sherinS216@gmail.com>
2024-03-15 14:10:14 -0400

    uploaded work
    function :onehotencoding, onehotdecoding, uppercase

--- Pipeline/onehotDecoding_sherin.py
+++ Pipeline/onehotDecoding_sherin.py
@@ -1,51 +1,14 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from xgboost import XGBClassifier
-
-
-# In[12]:
-
-
 import nbimporter
 import onehotEncoding_sherin
 
 
-# In[13]:
-
-
 df=pd.read_csv('fraud_detection.csv')
-
-
-# In[14]:
-
-
-df
-
-
-# In[15]:
-
-
 encoded_df=onehotEncoding_sherin.one_hot_encode(df)
-
-
-# In[16]:
-
-
-encoded_df
-
-
-# In[17]:
-
-
-import pandas as pd
 
 def one_hot_decode(encoded_df):
     
@@ -63,30 +26,13 @@ def one_hot_decode(encoded_df):
 
     return decoded_df
 
-
-
-# In[18]:
-
-
 encoded_df1 = pd.DataFrame(encoded_df)
-
-
-# In[19]:
-
-
-# Reverse one-hot encoding
 decoded_df = one_hot_decode(encoded_df1)
 
-
-# In[20]:
-
-
-# Display the decoded DataFrame
 print("Decoded DataFrame:")
 print(decoded_df)
 
 
-# In[ ]:
 
 
 
--- Pipeline/uppercase_sherin.py
+++ Pipeline/uppercase_sherin.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -11,47 +5,19 @@ import seaborn as sns
 from xgboost import XGBClassifier
 
 
-# In[2]:
-
 
 df=pd.read_csv('fraud_detection.csv')
-
-
-# In[3]:
-
-
-df
-
-
-# In[4]:
-
-
-#function to convert strings to uppercase
-
-
-# In[5]:
-
 
 def uppercase_strings(df):
     for column in df.columns:
         if df[column].dtype == 'object':
             df[column] = df[column].str.upper()
     return df
-
-
-# In[6]:
-
-
 new_df=uppercase_strings(df)
-
-
-# In[7]:
-
-
 new_df
 
 
-# In[ ]:
+
 
 
 

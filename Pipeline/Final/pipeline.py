a2d996dc285519b28715f2744d4b68e7820c7d2a
Leandro Sartini de Campos <leandro.sartini.c@gmail.com>
2024-04-12 15:08:52 -0400

    Change in final presentation

--- Pipeline/Final/pipeline.py
+++ Pipeline/Final/pipeline.py
@@ -64,35 +64,6 @@ def pipeline(data, filename):
     data.to_csv('../../data/predicted/Predicted_file.csv', index=False)
     return data
 
-
-def mid_sampling(X_train, Y_train, target_minority_samples=180000):
-    def oversample_with_adasyn(X, y, target_minority_samples=180000):
-        classes, counts = np.unique(y, return_counts=True)
-        minority_class = classes[np.argmin(counts)]
-        current_minority_samples = counts[np.argmin(counts)]
-        if current_minority_samples >= target_minority_samples:
-            raise ValueError("The minority class already has more or equal samples than the target_minority_samples.")
-        n_samples_to_generate = target_minority_samples - current_minority_samples
-        sampling_strategy = {minority_class: n_samples_to_generate}
-        adasyn = ADASYN(sampling_strategy=sampling_strategy, n_neighbors=5, random_state=42)
-        x_resampled, Y_resampled = adasyn.fit_resample(X, y)
-        return x_resampled, Y_resampled
-
-    def undersample_with_nearmiss(X, y):
-        classes, counts = np.unique(y, return_counts=True)
-        minority_class_count = min(counts)
-        nm = NearMiss()
-        x_resampled, Y_resampled = nm.fit_resample(X, y)
-        if len(np.unique(Y_resampled, return_counts=True)[1]) != 2 or np.unique(Y_resampled, return_counts=True)[1][
-            0] != minority_class_count:
-            raise ValueError("NearMiss did not undersample correctly.")
-        return x_resampled, Y_resampled
-
-    X_resampled, y_resampled = oversample_with_adasyn(X_train, Y_train, target_minority_samples)
-    X_train, Y_train = undersample_with_nearmiss(X_resampled, y_resampled)
-    return X_train, Y_train
-
-
 def main():
     df = pd.read_csv('../../data/processed/Sample_Data.csv')
 

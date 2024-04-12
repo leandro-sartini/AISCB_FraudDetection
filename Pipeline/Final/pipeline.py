6d4ac997e2b3bca16a31709575e64c5d808181de
Leandro Sartini de Campos <leandro.sartini.c@gmail.com>
2024-04-12 12:59:35 -0400

    Fixing Pipeline and jupyters

--- Pipeline/Final/pipeline.py
+++ Pipeline/Final/pipeline.py
@@ -1,16 +1,13 @@
 import pandas as pd
 import numpy as np
+from joblib import load
 from imblearn.over_sampling import ADASYN
 from imblearn.under_sampling import NearMiss
-from joblib import load
+from sklearn.model_selection import train_test_split, cross_val_score, KFold, RandomizedSearchCV
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.feature_selection import RFE
+from sklearn.linear_model import LogisticRegression
 import pickle
-
-
-def uppercase_strings(df):
-    for column in df.columns:
-        if df[column].dtype == 'object':
-            df[column] = df[column].str.upper()
-    return df
 
 
 def one_hot_encode(data):
@@ -44,13 +41,11 @@ def load_model(filename):
 
 
 def predict_data(data, cols, loaded_model):
-    data['Prediction'] = loaded_model.predict(data[cols])
+    data['Prediction'] = loaded_model.predict(data)
     return data
 
 
 def pipeline(data, filename):
-    print("Step 1:Uppercase Cols\n\n")
-    data = uppercase_strings(data)
     print("Step 2:One Hot\n\n")
     data = one_hot_encode(data)
     print("Step 3:Scale\n\n")
@@ -63,37 +58,35 @@ def pipeline(data, filename):
     data = predict_data(data, cols, loaded_model)
 
     data['ID'] = data.index
-    data['Probabilities'] = loaded_model.predict_proba(data[cols])[:, 1]
+    prediction_cols = data.drop(columns=['Prediction', 'ID']).columns
+    data['Probabilities'] = loaded_model.predict_proba(data[prediction_cols])[:, 1]
     data = data[['ID', 'Prediction', 'Probabilities']]
     data.to_csv('../../data/predicted/Predicted_file.csv', index=False)
     return data
 
 
-def oversample_with_adasyn(X, y, target_minority_samples=180000):
-    classes, counts = np.unique(y, return_counts=True)
-    minority_class = classes[np.argmin(counts)]
-    current_minority_samples = counts[np.argmin(counts)]
-    if current_minority_samples >= target_minority_samples:
-        raise ValueError("The minority class already has more or equal samples than the target_minority_samples.")
-    n_samples_to_generate = target_minority_samples - current_minority_samples
-    sampling_strategy = {minority_class: n_samples_to_generate}
-    adasyn = ADASYN(sampling_strategy=sampling_strategy, n_neighbors=5, random_state=42)
-    x_resampled, Y_resampled = adasyn.fit_resample(X, y)
-    return x_resampled, Y_resampled
-
-
-def undersample_with_nearmiss(X, y):
-    classes, counts = np.unique(y, return_counts=True)
-    minority_class_count = min(counts)
-    nm = NearMiss()
-    x_resampled, Y_resampled = nm.fit_resample(X, y)
-    if (len(np.unique(Y_resampled, return_counts=True)[1]) != 2 or
-            np.unique(Y_resampled, return_counts=True)[1][0] != minority_class_count):
-        raise ValueError("NearMiss did not undersample correctly.")
-    return x_resampled, Y_resampled
-
-
 def mid_sampling(X_train, Y_train, target_minority_samples=180000):
+    def oversample_with_adasyn(X, y, target_minority_samples=180000):
+        classes, counts = np.unique(y, return_counts=True)
+        minority_class = classes[np.argmin(counts)]
+        current_minority_samples = counts[np.argmin(counts)]
+        if current_minority_samples >= target_minority_samples:
+            raise ValueError("The minority class already has more or equal samples than the target_minority_samples.")
+        n_samples_to_generate = target_minority_samples - current_minority_samples
+        sampling_strategy = {minority_class: n_samples_to_generate}
+        adasyn = ADASYN(sampling_strategy=sampling_strategy, n_neighbors=5, random_state=42)
+        x_resampled, Y_resampled = adasyn.fit_resample(X, y)
+        return x_resampled, Y_resampled
+
+    def undersample_with_nearmiss(X, y):
+        classes, counts = np.unique(y, return_counts=True)
+        minority_class_count = min(counts)
+        nm = NearMiss()
+        x_resampled, Y_resampled = nm.fit_resample(X, y)
+        if len(np.unique(Y_resampled, return_counts=True)[1]) != 2 or np.unique(Y_resampled, return_counts=True)[1][
+            0] != minority_class_count:
+            raise ValueError("NearMiss did not undersample correctly.")
+        return x_resampled, Y_resampled
 
     X_resampled, y_resampled = oversample_with_adasyn(X_train, Y_train, target_minority_samples)
     X_train, Y_train = undersample_with_nearmiss(X_resampled, y_resampled)
@@ -102,8 +95,9 @@ def mid_sampling(X_train, Y_train, target_minority_samples=180000):
 
 def main():
     df = pd.read_csv('../../data/processed/Sample_Data.csv')
+
     # Continue with your pipeline...
-    pipeline(df, '../../models/fraud_model.sav')
+    pipeline(df, '../../models/best_logistic.sav')
 
 
 if __name__ == "__main__":
